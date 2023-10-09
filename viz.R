f14bb756fa2266b6768c8933687d718d7fcbe701
MatildaBae <146879342+MatildaBae@users.noreply.github.com>
2023-10-08 18:44:47 -0700

    Delete viz.R

--- viz.R
+++
@@ -1,9 +0,0 @@
-# install.packages("tidyverse")
-# install.packages("palmerpenguins")
-
-library(tidyverse)
-library(palmerpenguins)
-
-penguins |>
-  ggplot(aes(x= bill_depth_mm)) +
-  geom_histogram()
